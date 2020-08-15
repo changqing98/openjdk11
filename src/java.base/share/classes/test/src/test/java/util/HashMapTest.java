f2ae1c44f4a28fb42c9126ff0812314748ef373d
changqingyeh <yecq1998@outlook.com>
2020-08-15 23:48:49 +0800

    Add test demo

---
+++ src/java.base/share/classes/test/src/test/java/util/HashMapTest.java
@@ -0,0 +1,13 @@
+package test.java.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+class HashMapTest {
+  @Test
+  public void test() {
+    HashMap<String, String> map = new HashMap<>();
+    map.put("key", "values");
+  }
+}
